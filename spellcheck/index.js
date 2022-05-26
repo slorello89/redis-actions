7351b86f0d802a77f07f2c9db83266eb73fa61e3
slorello89 <slorello89@gmail.com>
2022-05-26 13:34:52 -0400

    some debug logic

--- spellcheck/index.js
+++ spellcheck/index.js
@@ -1,9 +1,13 @@
 const {Toolkit} = require("actions-toolkit");
 const childProcess = require("child_process");
 
-Toolkit.run(async => {
+Toolkit.run(async tools => {
     const dir = process.env.DOCS_DIRECTORY;
     const dictionary = process.env.DICTIONARY;
+    let ls = childProcess.execSync('ls -l')
+    tools.log.info(ls.toString());
+    ls = childProcess.execSync('ls -l /app')
+    tools.log.info(ls.toString());
     childProcess.execSync(`cp /app/${dictionary} /app/${dir}`);
     childProcess.exec(`cd /app/${dir} && spellchecker --no-suggestions -f '**/*.md' -l en-US -q -d ${dictionary} --plugins spell syntax-mentions syntax-urls`, (exception,out,err)=>{        
         console.log(err);
