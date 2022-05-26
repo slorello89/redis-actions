3cecbbefe029eff227c790df0171d5dabb2b8e2e
slorello89 <slorello89@gmail.com>
2022-05-26 13:42:25 -0400

    backwards

--- spellcheck/index.js
+++ spellcheck/index.js
@@ -3,12 +3,12 @@ const childProcess = require("child_process");
 
 Toolkit.run(async tools => {
     const dir = process.env.DOCS_DIRECTORY;
-    const dictionary = process.env.DICTIONARY;
+    const dictionary = process.env.DICTIONARY == null ? "default_dictionary.txt" : process.env.DICTIONARY;
     childProcess.execSync(`cp /app/${dictionary} ./${dir}`);    
     childProcess.exec(`cd ./${dir} && spellchecker --no-suggestions -f '**/*.md' -l en-US -q -d ${dictionary} --plugins spell syntax-mentions syntax-urls`, (exception,out,err)=>{        
-        tools.info.log(err);
-        tools.info.log(out);
-        tools.info.log(exception)
+        tools.log.info(err);
+        tools.log.info(out);
+        tools.log.info(exception)
     });
     
 })
