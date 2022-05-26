c4a6f60ab07613eed0f2f0ad6c28ee095d287617
slorello89 <slorello89@gmail.com>
2022-05-26 16:59:44 -0400

    adding exception for when docs directory is root

--- spellcheck/index.js
+++ spellcheck/index.js
@@ -5,7 +5,11 @@ const core = require("@actions/core")
 Toolkit.run(async tools => {
     const dir = process.env.DOCS_DIRECTORY;
     const dictionary = process.env.DICTIONARY == null ? "/app/default_dictionary.txt" : process.env.DICTIONARY;
-    childProcess.execSync(`cp ${dictionary} ./${dir}`);    
+    if(dir != '.')
+    {
+        childProcess.execSync(`cp ${dictionary} ./${dir}`);    
+    }
+        
     childProcess.exec(`cd ./${dir} && spellchecker --no-suggestions -f '**/*.md' -l en-US -q -d ${dictionary}`, (exception,out,err)=>{        
         tools.log.info(err);        
         if(out){
