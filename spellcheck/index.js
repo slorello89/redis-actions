354e5a1461187a7773ec25b174d2f6cf2eea03f7
slorello89 <slorello89@gmail.com>
2022-05-26 14:29:33 -0400

    fixing path

--- spellcheck/index.js
+++ spellcheck/index.js
@@ -3,12 +3,11 @@ const childProcess = require("child_process");
 
 Toolkit.run(async tools => {
     const dir = process.env.DOCS_DIRECTORY;
-    const dictionary = process.env.DICTIONARY == null ? "default_dictionary.txt" : process.env.DICTIONARY;
-    childProcess.execSync(`cp /app/${dictionary} ./${dir}`);    
+    const dictionary = process.env.DICTIONARY == null ? "/app/default_dictionary.txt" : process.env.DICTIONARY;
+    childProcess.execSync(`cp ${dictionary} ./${dir}`);    
     childProcess.exec(`cd ./${dir} && spellchecker --no-suggestions -f '**/*.md' -l en-US -q -d ${dictionary} --plugins spell syntax-mentions syntax-urls`, (exception,out,err)=>{        
         tools.log.info(err);
         tools.log.info(out);
         tools.log.info(exception)
-    });
-    
+    });    
 })
