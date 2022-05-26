baf722d41b142f4ea06767c2db2c29d2c0c5f2a5
slorello89 <slorello89@gmail.com>
2022-05-26 13:28:36 -0400

    fixing dictionary directory

--- spellcheck/index.js
+++ spellcheck/index.js
@@ -4,8 +4,8 @@ const childProcess = require("child_process");
 Toolkit.run(async => {
     const dir = process.env.DOCS_DIRECTORY;
     const dictionary = process.env.DICTIONARY;
-    childProcess.execSync(`cp ${dictionary} ./${dir}`);
-    childProcess.exec(`cd ${dir} && spellchecker --no-suggestions -f '**/*.md' -l en-US -q -d ${dictionary} --plugins spell syntax-mentions syntax-urls`, (exception,out,err)=>{        
+    childProcess.execSync(`cp /app/${dictionary} /app/${dir}`);
+    childProcess.exec(`cd /app/${dir} && spellchecker --no-suggestions -f '**/*.md' -l en-US -q -d ${dictionary} --plugins spell syntax-mentions syntax-urls`, (exception,out,err)=>{        
         console.log(err);
         console.log(out);
         console.log(exception)
