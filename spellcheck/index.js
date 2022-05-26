ca2160f4f6aa08018823cafe38ebf13422ccd2b8
slorello89 <slorello89@gmail.com>
2022-05-26 14:41:31 -0400

    removing plugin statements

--- spellcheck/index.js
+++ spellcheck/index.js
@@ -6,7 +6,7 @@ Toolkit.run(async tools => {
     const dir = process.env.DOCS_DIRECTORY;
     const dictionary = process.env.DICTIONARY == null ? "/app/default_dictionary.txt" : process.env.DICTIONARY;
     childProcess.execSync(`cp ${dictionary} ./${dir}`);    
-    childProcess.exec(`cd ./${dir} && spellchecker --no-suggestions -f '**/*.md' -l en-US -q -d ${dictionary} --plugins spell syntax-mentions syntax-urls`, (exception,out,err)=>{        
+    childProcess.exec(`cd ./${dir} && spellchecker --no-suggestions -f '**/*.md' -l en-US -q -d ${dictionary}, (exception,out,err)=>{        
         tools.log.info(err);        
         if(out){
             core.setFailed(out);
