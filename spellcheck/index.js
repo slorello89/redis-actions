1c6485d6e98982302a13a28ef2373f677550546d
slorello89 <slorello89@gmail.com>
2022-05-26 14:35:39 -0400

    trying setFailed

--- spellcheck/index.js
+++ spellcheck/index.js
@@ -7,10 +7,10 @@ Toolkit.run(async tools => {
     childProcess.execSync(`cp ${dictionary} ./${dir}`);    
     childProcess.exec(`cd ./${dir} && spellchecker --no-suggestions -f '**/*.md' -l en-US -q -d ${dictionary} --plugins spell syntax-mentions syntax-urls`, (exception,out,err)=>{        
         tools.log.info(err);        
-        if(out || exception){
-            tools.exit = 1
+        if(out){
+            core.setFailed(out);
         }
-        tools.log.info(out);
+        
         tools.log.info(exception)
     });    
 })
