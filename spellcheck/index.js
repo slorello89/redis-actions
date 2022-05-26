b1b729c1213d4a1b2e0c118fadc6e05227aa6d45
slorello89 <slorello89@gmail.com>
2022-05-26 13:40:06 -0400

    removing debugging code

--- spellcheck/index.js
+++ spellcheck/index.js
@@ -4,13 +4,7 @@ const childProcess = require("child_process");
 Toolkit.run(async tools => {
     const dir = process.env.DOCS_DIRECTORY;
     const dictionary = process.env.DICTIONARY;
-    childProcess.execSync(`cp /app/${dictionary} ./${dir}`);
-    let ls = childProcess.execSync('ls -l')
-    tools.log.info(ls.toString());
-    ls = childProcess.execSync('ls -l /app')
-    tools.log.info(ls.toString());    
-    ls = childProcess.execSync(`ls -l ./${dir}`)
-    tools.log.info(ls.toString());    
+    childProcess.execSync(`cp /app/${dictionary} ./${dir}`);    
     childProcess.exec(`cd ./${dir} && spellchecker --no-suggestions -f '**/*.md' -l en-US -q -d ${dictionary} --plugins spell syntax-mentions syntax-urls`, (exception,out,err)=>{        
         console.log(err);
         console.log(out);
