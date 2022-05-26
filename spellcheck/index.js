3048b75b85c83981b355eb8c2708490678b9f177
slorello89 <slorello89@gmail.com>
2022-05-26 13:36:36 -0400

    changing copy logic

--- spellcheck/index.js
+++ spellcheck/index.js
@@ -4,11 +4,11 @@ const childProcess = require("child_process");
 Toolkit.run(async tools => {
     const dir = process.env.DOCS_DIRECTORY;
     const dictionary = process.env.DICTIONARY;
+    childProcess.execSync(`cp /app/${dictionary} ./${dir}`);
     let ls = childProcess.execSync('ls -l')
     tools.log.info(ls.toString());
     ls = childProcess.execSync('ls -l /app')
-    tools.log.info(ls.toString());
-    childProcess.execSync(`cp /app/${dictionary} /app/${dir}`);
+    tools.log.info(ls.toString());    
     childProcess.exec(`cd /app/${dir} && spellchecker --no-suggestions -f '**/*.md' -l en-US -q -d ${dictionary} --plugins spell syntax-mentions syntax-urls`, (exception,out,err)=>{        
         console.log(err);
         console.log(out);
