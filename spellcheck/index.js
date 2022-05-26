fb66601184d59a67bbd286762f46ccb4151f52b9
slorello89 <slorello89@gmail.com>
2022-05-26 13:37:04 -0400

    more debugging

--- spellcheck/index.js
+++ spellcheck/index.js
@@ -9,6 +9,8 @@ Toolkit.run(async tools => {
     tools.log.info(ls.toString());
     ls = childProcess.execSync('ls -l /app')
     tools.log.info(ls.toString());    
+    ls = childProcess.execSync(`ls -l ./${dir}`)
+    tools.log.info(ls.toString());    
     childProcess.exec(`cd /app/${dir} && spellchecker --no-suggestions -f '**/*.md' -l en-US -q -d ${dictionary} --plugins spell syntax-mentions syntax-urls`, (exception,out,err)=>{        
         console.log(err);
         console.log(out);
