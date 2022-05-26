de6163ab4bbe3d1a3fbb3708ead94f92419530b6
slorello89 <slorello89@gmail.com>
2022-05-26 14:32:34 -0400

    failing on failur

--- spellcheck/index.js
+++ spellcheck/index.js
@@ -6,7 +6,10 @@ Toolkit.run(async tools => {
     const dictionary = process.env.DICTIONARY == null ? "/app/default_dictionary.txt" : process.env.DICTIONARY;
     childProcess.execSync(`cp ${dictionary} ./${dir}`);    
     childProcess.exec(`cd ./${dir} && spellchecker --no-suggestions -f '**/*.md' -l en-US -q -d ${dictionary} --plugins spell syntax-mentions syntax-urls`, (exception,out,err)=>{        
-        tools.log.info(err);
+        tools.log.info(err);        
+        if(out || exception){
+            tools.exit = 1
+        }
         tools.log.info(out);
         tools.log.info(exception)
     });    
